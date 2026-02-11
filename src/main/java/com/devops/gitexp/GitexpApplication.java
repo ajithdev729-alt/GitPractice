478fd4a501137da5399829200881b82e839f4d82
Ajithdev <ajithdev729@gmail.com>
2026-02-11 10:47:42 -0500

    my first actual commit

---
+++ src/main/java/com/devops/gitexp/GitexpApplication.java
@@ -0,0 +1,13 @@
+package com.devops.gitexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitexpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitexpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/gitexp/GitexpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.gitexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitexpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
